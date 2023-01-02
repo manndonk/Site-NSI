d38856b8889985a896b442d12d2e0964f919ac17
Flaott <68269043+Flaott@users.noreply.github.com>
2023-01-02 18:53:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// document.getElementById("intro").style.height = window.innerHeight.toString() + "px";
+// document.getElementById("bowie").style.height = window.innerHeight.toString() + "px";
+// document.getElementById("bowie").style.top = window.innerHeight.toString() + "px";
+// console.log(window.innerWidth);
