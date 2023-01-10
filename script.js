5bd9cbdb57eb4a18fe54dac66af3dfa047000354
manndonk <68269043+manndonk@users.noreply.github.com>
2023-01-10 03:16:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let page = 0;
 function toggleDisplay(event) {
-    if (event.deltaY < 0) {
-        if (page != 0){
-            page += 100; 
-        }
-        document.querySelector("body").style.transform = "translateY(" + page.toString() + "vh)";
+  if (event.deltaY < 0) {
+    if (page != 0) {
+      page += 100;
     }
-    else {
-        if (page != -200){
-            page -= 100; 
-        }
-        document.querySelector("body").style.transform = "translateY(" + page.toString() + "vh)";
+    document.querySelector("body").style.transform = "translateY(" + page.toString() + "vh)";
+  }
+  else {
+    if (page != -200) {
+      page -= 100;
     }
+    document.querySelector("body").style.transform = "translateY(" + page.toString() + "vh)";
+  }
 }
 
 function retourne() {
-    page = -200;
-    document.querySelector("section:nth-child(2)").scrollIntoView();
+  page = -200;
+  document.querySelector("section:nth-child(2)").scrollIntoView();
 }
 
 window.addEventListener('wheel', toggleDisplay);
